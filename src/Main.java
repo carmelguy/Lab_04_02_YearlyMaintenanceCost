f44c485ad0c1356ad6d9d0e32ab19d2cb4f9bdae
Zach <zach.gause@gmail.com>
2025-02-04 00:20:08 -0500

    Yearly Maintenance Cost commit

---
+++ src/Main.java
@@ -0,0 +1,22 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main
+{
+    public static void main(String[] args)
+    {
+        double springCost = 100.00;
+        double summerCost = 75.50;
+        double fallCost = 150.75;
+        double winterCost = 200.25;
+        double yearlyTotal = springCost + summerCost + fallCost + winterCost;
+
+        System.out.println("Spring maintenance cost: $" + springCost);
+        System.out.println("Summer maintenance cost: $" + summerCost);
+        System.out.println("Fall maintenance cost: $" + fallCost);
+        System.out.println("Winter maintenance cost: $" + winterCost);
+        System.out.println("Total yearly maintenance cost: $" + yearlyTotal);
+
+
+    }
+
+}
